cd610797d7e3d89980623b6c9d98c7ce32fcf665
crasmek <itaysen@gmail.com>
2020-11-16 20:12:02 +0200

    Updates

---
+++ precache-manifest.00a324bf94218baf420227e0237c4dbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967adfccc90d080e4a57384a2d256d48",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "e57d6fa5eb287ac595c5",
+    "url": "/project/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "63c009152d7a117d263e",
+    "url": "/project/static/css/main.b2a6b74d.chunk.css"
+  },
+  {
+    "revision": "e57d6fa5eb287ac595c5",
+    "url": "/project/static/js/2.6cd6baaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project/static/js/2.6cd6baaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63c009152d7a117d263e",
+    "url": "/project/static/js/main.05b6a3fc.chunk.js"
+  },
+  {
+    "revision": "34e1c33a93ef1416ebd6",
+    "url": "/project/static/js/runtime-main.580aa577.js"
+  }
+]);
